05bc09765f49871db49754611d1bc4a27e758e8a
Igor M.A <56927248+alves-dev@users.noreply.github.com>
2020-07-25 00:54:15 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='graphics',
+      version='0.1',
+      description='graphics',
+      url='https://github.com/alves-dev/graficos',
+      author='Igor Moreira',
+      author_email='alvesmoreiraigor@gmail.com',
+      license='MIT',
+      packages=['files','graphics','my_time','netflix','rules'],
+      zip_safe=False)
