0234a5321c691a631c27c1bb17909e3f9358339f
Igor Moreira <alvesmoreiraigor@gmail.com>
2020-07-25 01:26:09 -0300

    setup.py Alterado

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='graphics',
+setup(name='graficos',
       version='0.1',
-      description='graphics',
+      description='Geração de graficos',
       url='https://github.com/alves-dev/graficos',
       author='Igor Moreira',
       author_email='alvesmoreiraigor@gmail.com',
       license='MIT',
-      packages=['files','graphics','my_time','netflix','rules'],
+      packages=['files', 'graphics', 'my_time', 'netflix', 'rules'],
       zip_safe=False)
