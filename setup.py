d85227f705ebdde83e54bf4c10a9e30140911f78
Igor Moreira <alvesmoreiraigor@gmail.com>
2020-10-10 08:04:42 -0300

    new version 1.1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='graficos',
-      version='1.0',
+      version='1.1.0',
       description='Geração de graficos',
       url='https://github.com/alves-dev/graficos',
       author='Igor Moreira',
       author_email='alvesmoreiraigor@gmail.com',
       license='MIT',
-      packages=['files', 'graphics', 'main', 'my_time', 'netflix', 'rules', 'utility'],
+      packages=['files', 'graphics', 'main', 'my_time', 'netflix', 'validations', 'utility'],
       zip_safe=False)
